fb93948453e4ecb0454e12c0fbe57e3a043a30e4
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2021-12-01 09:27:16 -0300

    Criando um novo branch e atualizando o trabalhoP2P

--- trabalhoP2P.py
+++ trabalhoP2P.py
@@ -19,7 +19,20 @@ def cliente(udp):
     while True:
         msg, cliente = udp.recvfrom(1024)
         msg_decoded = msg.decode("utf-8")
-        print(f"{msg_decoded}\n")
+        print("\n")
+        print(json.dumps(json.loads(msg_decoded), indent=4))
+        # Esta dnado erro aki \/
+        # node["ip_antecessor"] = str(msg_decoded["ip_antecessor"])
+        # node["ip_sucessor"] = str(msg_decoded["ip_sucessor"])
+        # informacoes_do_no = f"""
+        #                ID: {node['id']}
+        #                IP: {node['ip']}
+        #     ID_ANTECESSOR: {node['id_antecessor']}
+        #     IP_ANTECESSOR: {node['ip_antecessor']}
+        #       ID_SUCESSOR: {node['id_sucessor']}
+        #       IP_SUCESSOR: {node['ip_sucessor']}
+        # """
+        # print(informacoes_do_no)
     udp.close()
 
 
@@ -27,8 +40,6 @@ def servidor(udp):
     while True:
         msg, cliente = udp.recvfrom(1024)
         msg_decoded = msg.decode("utf-8")
-        print(msg_decoded)
-        print(cliente)
         if msg_decoded:
             msg_json = (json.loads(msg_decoded))
             if msg_json["codigo"] == 0:
@@ -36,17 +47,15 @@ def servidor(udp):
                 mensagem['codigo'] = 64
                 if node["id_sucessor"] == node["id_antecessor"] == node["id"]:
                     if node["id"] < msg_json["id"]:
-                        ''' ///////////// Aki esta dando errado /////////////'''
-                        mensagem["id_sucesso"] = node["id_sucesso"]
+                        mensagem["id_sucessor"] = node["id_sucessor"]
                         mensagem["ip_sucessor"] = node["ip_sucessor"]
                         mensagem["id_antecessor"] = node["id_antecessor"]
                         mensagem["ip_antecessor"] = node["ip_antecessor"]
                     else:
-                        mensagem["id_sucesso"] = node["id_antecessor"]
+                        mensagem["id_sucessor"] = node["id_antecessor"]
                         mensagem["ip_sucessor"] = node["ip_antecessor"]
-                        mensagem["id_antecessor"] = node["id_sucesso"]
+                        mensagem["id_antecessor"] = node["id_sucessor"]
                         mensagem["ip_antecessor"] = node["ip_sucessor"]
-                        ''' ///////////////////////////////////////////////////////'''
                 mensage = json.dumps(mensagem)
                 udp.sendto(mensage.encode("utf-8"), cliente)
 
@@ -60,6 +69,7 @@ def main():
     while opc != "6":
         informacoes_do_no = f"""
                        ID: {node['id']}
+                       IP: {node['ip']}
             ID_ANTECESSOR: {node['id_antecessor']}
             IP_ANTECESSOR: {node['ip_antecessor']}
               ID_SUCESSOR: {node['id_sucessor']}
@@ -75,14 +85,15 @@ def main():
         print("# 6 - Sair da aplicacao")
         print("#####################################")
         opc = input("Informe uma opcao: ")
+        print("\n" * 130)
         if opc == "1":
-            node['id'] = random.randrange(1, 1000)
+            node['id'] = str(random.randrange(1, 1000))
         elif opc == "2":
             node['id_antecessor'] = node['id']
             node['id_sucessor'] = node['id']
             node['ip_antecessor'] = node['ip']
             node['ip_sucessor'] = node['ip']
-            _thread.start_new_thread(servidor, (udp,))
+            _thread.start_new_thread(servidor, (udp, ))
         elif opc == "3":
             endereco_lookup = input("informe o endereco IP: ")
             print(endereco_lookup)
